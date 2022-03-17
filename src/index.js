3fd037658d6cb2b011d29ba9ae55cd001aa56074
Luiz Pires <lpiresjunior@gmail.com>
2022-03-17 01:49:02 -0300

    create customer account

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello World!" });
+app.use(express.json());
+
+const customers = [];
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+  const id = uuidv4();
+
+  customers.push({
+    cpf,
+    name,
+    id,
+    statament: []
+  });
+
+  return response.status(201).send();
 });
 
 app.listen(3333);
