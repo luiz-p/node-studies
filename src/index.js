df92234ddededade22abd1bab2d31038a938105c
Luiz Pires <lpiresjunior@gmail.com>
2022-03-22 08:13:04 -0300

    create account deposit

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ app.post("/account", (request, response) => {
     cpf,
     name,
     id: uuidv4(),
-    statament: []
+    statement: []
   });
 
   return response.status(201).send();
@@ -43,7 +43,25 @@ app.post("/account", (request, response) => {
 app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
   const { customer } = request;
 
-  return response.json(customer.statament)
+  return response.json(customer.statement)
+});
+
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+
+
+  const { customer } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit"
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
 });
 
 app.listen(3333);
